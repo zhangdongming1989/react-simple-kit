e00748bb6ae000ec5752279cdb5d68bb29b9d30f
zhangdongming <zhangdongming@zhihu.com>
2019-01-12 22:08:58 +0800

    feat: init commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>react app</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
