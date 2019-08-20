8c85610cbe639e1e9b5bca0e51c6a1f1f248366f
zhangdongming <zhangdongming@zhihu.com>
2019-08-20 19:02:03 +0800

    chore: upgrade all deps and add style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/app.js',
+  entry: "./src/app.js",
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + "/dist",
     filename: "dist.js"
   },
   module: {
-    rules: [{
-      test: /\.js/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+    rules: [
+      {
+        test: /\.js/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"]
+        }
       },
-    }]
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html'
+      filename: "index.html",
+      template: "src/index.html"
     })
   ]
-}
+};
